a9a7ffc94d8560c5fa6e41ec6c32a51298c70ab0
antonovmike <antonovma@outlook.com>
2024-01-08 17:01:03 +0400

    Lesson 6 Create tables does not work

--- lesson_6/app/keyboards.py
+++ lesson_6/app/keyboards.py
@@ -7,7 +7,7 @@ button_3 = KeyboardButton(text='Contacts')
 button_4 = KeyboardButton(text='Admin keyboard')
 raw_1 = [button_1, button_2, button_3]
 raw_2 = [button_4]
-kb = ReplyKeyboardMarkup(keyboard=[raw_1], resize_keyboard=True)
+kb_user = ReplyKeyboardMarkup(keyboard=[raw_1], resize_keyboard=True)
 kb_add = ReplyKeyboardMarkup(keyboard=[raw_1, raw_2], resize_keyboard=True)
 button_5 = KeyboardButton(text='Add goods')
 button_6 = KeyboardButton(text='Delete goods')
@@ -17,7 +17,7 @@ kb_admin = ReplyKeyboardMarkup(keyboard=[raw_1], resize_keyboard=True)
 
 inline_button_1 = InlineKeyboardButton(text='Shirts', url='duckduckgo.com')
 inline_button_2 = InlineKeyboardButton(text='T-Shirts', url='duckduckgo.com')
-inline_button_3 = InlineKeyboardButton(text='Sneakers', callback_data='Sneakers')
+inline_button_3 = InlineKeyboardButton(text='Sneakers', callback_data='sneakers')
 inline_raw_1 = [inline_button_1, inline_button_2]
 inline_raw_2 = [inline_button_3]
 catalog_list = InlineKeyboardMarkup(inline_keyboard=[inline_raw_1, inline_raw_2])
--- lesson_6/main.py
+++ lesson_6/main.py
@@ -8,7 +8,8 @@ from aiogram.filters import CommandStart
 from aiogram.types import Message
 from dotenv import load_dotenv
 
-from app.keyboards import kb, kb_add, kb_admin, catalog_list
+from app import database as db
+from app import keyboards as kb
 
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
@@ -16,26 +17,31 @@ bot = Bot(TOKEN)
 dp = Dispatcher(bot=bot)
 
 
+async def on_startup():
+    await db.db_start()
+    print('DB connected')
+
+
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
     sticker_id = os.getenv('STICKER_ID')
     await message.answer_sticker(sticker_id)
     if message.from_user.id == int(os.getenv('ADMIN_ID')):
-        await message.answer(f'You are logged in as an administrator', reply_markup=kb_add)
+        await message.answer(f'You are logged in as an administrator', reply_markup=kb.kb_add)
     else:
-        await message.answer(f'Hello {message.from_user.full_name}', reply_markup=kb)
+        await message.answer(f'Hello {message.from_user.full_name}', reply_markup=kb.kb_user)
 
 
 @dp.message()
 async def buttons_click(message: Message):
     if message.text == 'Catalogue':
-        await message.answer(f'Here is our Catalogue', reply_markup=catalog_list)
+        await message.answer(f'Here is our Catalogue', reply_markup=kb.catalog_list)
     elif message.text == 'Card':
         await message.answer(f'Added to card')
     elif message.text == 'Contacts':
         await message.answer(f'Buy here: @no_user')
     elif message.from_user.id == int(os.getenv('ADMIN_ID')) and message.text == 'Admin keyboard':
-        await message.answer(f'You have opened the admin keyboard', reply_markup=kb_admin)
+        await message.answer(f'You have opened the admin keyboard', reply_markup=kb.kb_admin)
     elif message.text == 'id':
         await message.answer(f'{message.from_user.id}')
     else:
@@ -44,4 +50,4 @@ async def buttons_click(message: Message):
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
-    asyncio.run(dp.start_polling(bot))
+    asyncio.run(dp.start_polling(bot, on_startup=on_startup))
