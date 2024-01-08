ea884ae1ea7abac386e4d2318f9f0e3a69d0e5c9
antonovmike <antonovma@outlook.com>
2024-01-08 09:51:29 +0400

    Lesson 4 admin keyboard

--- lesson_4/main.py
+++ lesson_4/main.py
@@ -5,7 +5,9 @@ import os
 
 from aiogram import Bot, Dispatcher
 from aiogram.filters import CommandStart
-from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton
+from aiogram.filters.callback_data import CallbackData
+from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton, CallbackQuery
+from aiogram.utils.callback_answer import CallbackAnswerMiddleware
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -16,16 +18,25 @@ dp = Dispatcher(bot=bot)
 button_1 = KeyboardButton(text='Catalogue')
 button_2 = KeyboardButton(text='Card')
 button_3 = KeyboardButton(text='Contacts')
-raw_1 = [button_1, button_2]
-raw_2 = [button_3]
-kb = ReplyKeyboardMarkup(keyboard=[raw_1, raw_2], resize_keyboard=True)
+button_4 = KeyboardButton(text='Admin keyboard')
+raw_1 = [button_1, button_2, button_3]
+raw_2 = [button_4]
+kb = ReplyKeyboardMarkup(keyboard=[raw_1], resize_keyboard=True)
+kb_add = ReplyKeyboardMarkup(keyboard=[raw_1, raw_2], resize_keyboard=True)
+button_5 = KeyboardButton(text='Add goods')
+button_6 = KeyboardButton(text='Delete goods')
+button_7 = KeyboardButton(text='Mail campaign')
+raw_1 = [button_5, button_6, button_7]
+kb_admin = ReplyKeyboardMarkup(keyboard=[raw_1], resize_keyboard=True)
 
 
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
-    sticker_id = 'CAACAgIAAxkBAAIUSWWalI3UK4cUW2s25m49M2WlW6SZAAI7AQACijc4AAGSEIzViMEnBDQE'
+    sticker_id = os.getenv('STICKER_ID')
     await message.answer_sticker(sticker_id)
     await message.answer(f'Hello {message.from_user.full_name}', reply_markup=kb)
+    if str(message.from_user.id) == os.getenv('ADMIN_ID'):
+        await message.answer(f'you are logged in as an administrator', reply_markup=kb_add)
 
 
 @dp.message()
@@ -36,6 +47,10 @@ async def buttons_click(message: Message):
         await message.answer(f'Added to card')
     elif message.text == 'Contacts':
         await message.answer(f'Buy here: @no_user')
+    elif message.text == 'Admin keyboard':
+        await message.answer(f'You have opened the admin keyboard')
+    if message.text == 'id':
+        await message.answer(f'{message.from_user.id}')
 
 
 @dp.message()
