5200a7f94a573353c14a85bf8ceb7666c204298c
antonovmike <antonovma@outlook.com>
2024-01-08 11:14:34 +0400

    Lesson 5 InlineKeyboardButton

--- lesson_5/main.py
+++ lesson_5/main.py
@@ -27,11 +27,12 @@ button_7 = KeyboardButton(text='Mail campaign')
 raw_1 = [button_5, button_6, button_7]
 kb_admin = ReplyKeyboardMarkup(keyboard=[raw_1], resize_keyboard=True)
 
-inline_button_1 = InlineKeyboardButton(text='Shirts', callback_data='Shirts')
-inline_button_2 = InlineKeyboardButton(text='T-Shirts', callback_data='T-Shirts')
+inline_button_1 = InlineKeyboardButton(text='Shirts', url='duckduckgo.com')
+inline_button_2 = InlineKeyboardButton(text='T-Shirts', url='duckduckgo.com')
 inline_button_3 = InlineKeyboardButton(text='Sneakers', callback_data='Sneakers')
-inline_raw_1 = [inline_button_1, inline_button_2, inline_button_3]
-catalog_list = InlineKeyboardMarkup(inline_keyboard=[inline_raw_1])
+inline_raw_1 = [inline_button_1, inline_button_2]
+inline_raw_2 = [inline_button_3]
+catalog_list = InlineKeyboardMarkup(inline_keyboard=[inline_raw_1, inline_raw_2])
 
 
 @dp.message(CommandStart())
